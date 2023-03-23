9cb52ee07a21677b38e021605225583b927c0cfc
unknown <vmadhesh422@gmail.com>
2023-03-23 14:48:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Reference Data Types use same memory</h1>
+       <button onClick={window["run"]}>click here</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+  
